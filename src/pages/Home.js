1d1e0d600b9b21bd693f7f4fc1fdd74a8749521b
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-07-14 01:19:23 +0600

    home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Header from '../shared/Header'
 
+
 const Home = () => {
   return (
     <div>
-        <Header></Header>
+  <Header></Header>
     </div>
   )
 }
